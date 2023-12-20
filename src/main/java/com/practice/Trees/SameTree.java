8d5f84656ee840183dc667afb86dca16efc02152
Salman Iraqui <salman.iraqui@blr.cordis>
2023-12-20 16:50:53 +0530

    Added: SameTree.java

---
+++ src/main/java/com/practice/Trees/SameTree.java
@@ -0,0 +1,34 @@
+package com.practice.Trees;
+
+/**
+ * @author salman.iraqui
+ *
+ */
+
+/*
+ * https://leetcode.com/problems/same-tree/description/
+ */
+
+public class SameTree {
+	
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+      	TreeNode() {}
+      	TreeNode(int val) { this.val = val; }
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null || q==null)
+            return (p==q);
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
