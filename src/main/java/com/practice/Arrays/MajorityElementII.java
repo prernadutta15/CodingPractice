3e6683457d7418690a635e666aac5ca29da72c2e
prernadutta15 <prerna46_scs@jnu.ac.in>
2024-02-10 20:03:34 +0530

    Added: MajorityElementII link

---
+++ src/main/java/com/practice/Arrays/MajorityElementII.java
@@ -0,0 +1,17 @@
+package com.practice.Arrays;
+
+/**
+ * @author salman.iraqui
+ *
+ */
+
+/*
+ * https://leetcode.com/problems/majority-element-ii/description/
+ */
+
+public class MajorityElementII {
+	public int majorityElementII(int[] nums) {
+        
+        return -1;
+    }
+}
